4e9bff93195e9f6f7b36b29cf688ebc1d6e6cc1a
Vitor Targino <vitortargino@MacBook-Air-de-Vitor.local>
2025-03-24 20:33:49 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/craterometro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.craterometro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:craterometro/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Craterometro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late MapController mapController;
+  bool showWall = true;
+
+  @override
+  void initState() {
+    super.initState();
+    mapController = MapController(
+      initPosition: GeoPoint(
+        latitude: -6.11198,
+        longitude: -38.20528,
+      ),
+      areaLimit: BoundingBox(
+        east: -38.2000,
+        west: -38.2500,
+        north: -6.1000,
+        south: -6.1500,
+      ),
+      useExternalTracking: true,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 10,
+        backgroundColor: Colors.white,
+      ),
+      body: Stack(
+        children: [
+          OSMFlutter(
+            controller: mapController,
+            osmOption: OSMOption(
+              showZoomController: true,
+              zoomOption: ZoomOption(
+                initZoom: 16,
+                minZoomLevel: 11,
+              ),
+            ),
+          ),
+          showWall
+              ? Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.transparent,
+                        Color.fromARGB(233, 0, 0, 0),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Ajude a mapear os problemas das ruas!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Registre buracos na cidade e contribua para ruas mais seguras e bem cuidadas.',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(250, 50),
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Comece Agora',
+                                style: TextStyle(fontSize: 22)),
+                            Icon(
+                              Icons.arrow_forward,
+                              size: 20,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            showWall = false;
+                          });
+                        },
+                        child: Text(
+                          'Continuar sem login',
+                          style: TextStyle(
+                              color: ThemeColors.primaryColor,
+                              decoration: TextDecoration.underline,
+                              decorationThickness: 2,
+                              decorationColor: ThemeColors.primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
+}
