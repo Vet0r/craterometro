da081ce3afcd11ed7cb49f661827a67c3a4c9063
Vitor Targino <vitortargino@MacBook-Air-de-Vitor.local>
2025-03-24 22:58:44 -0300

    icone e firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:craterometro/firebase_options.dart';
 import 'package:craterometro/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
